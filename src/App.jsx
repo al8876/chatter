18e7536a51744143827e186d797292513e7d58fe
al8876 <al8876@gmail.com>
2018-02-20 03:29:50 +0000

    React components for chatbar, nav bar, and message list created and renders

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,58 @@
 import React, {Component} from 'react';
-
+import Navbar from './Navbar.jsx';
+import Chatbar from './Chatbar.jsx';
+import MessageList from './MessageList.jsx';
+  
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: 'Anon',
+      messages: [{
+        id: 1,
+        type: 'system',
+        text: 'Anonymous changed name to Bob'
+      }, {
+        id: 2,
+        type: 'user',
+        text: 'this is a test',
+        user: 'bob'
+      }, {
+        id: 3,
+        type: 'system',
+        text: 'Anonymous changed name to Susan'
+      }, {
+        id: 4,
+        type: 'user',
+        text: 'hot dog',
+        user: 'susan'
+      }]
+    };
+  }
+
+    newMessage(messageText) {
+    const newMessageObject = {
+      id: Math.random(),
+      type: 'user',
+      user: this.state.user,
+      text: messageText
+    };
+    const newMessages = this.state.messages.concat(newMessageObject);
+    this.setState({
+      messages: newMessages
+    });
+  }
+
   render() {
+    console.log("Rendering <App/>");
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <Navbar />
+        <MessageList messages={this.state.messages}/>
+        <Chatbar newMessage={this.newMessage.bind(this)}/>
+      </div>
     );
   }
 }
+
 export default App;
