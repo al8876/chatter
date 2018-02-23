76411927ad9dcdd062b6eada8fc79b24fd51d1e2
al8876 <al8876@gmail.com>
2018-02-23 00:25:12 +0000

    on GET shows loading animation for 3 seconds

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,74 +1,107 @@
 import React, {Component} from 'react';
 
-class MessageList extends Component {
-  render(){
-    const messages = this.props.messages.map((message) => {
-
-      // Style for username color
-      let divStyle = {
-        color: message.color
-      };
-      
-      if (message.type === 'incomingMessage') {
-
-        // Convert incoming message text into an array
-        let arrayOfText = message.text.split(' ');
-
-        // Find the last value of the message text array
-        let endOfArray = arrayOfText[arrayOfText.length -1];
-
-        // Image Extension Variables
-        let gif = 'gif';
-        let png = 'png';
-        let jpg = 'jpg';
-
-        // If last text element ends with one of Image Extension Variables
-        if (endOfArray.endsWith(gif) || endOfArray.endsWith(png) || endOfArray.endsWith(jpg)) {
-
-          // Variable to remove link from text
-          let sliceArray = arrayOfText.slice(0, -1)
-
-          // Message without the image URL attached
-          let renderMessage = sliceArray.join(' ');
-
-          return (
-            <div key={message.id} className='message'>
-              <span className='message-username' style={divStyle}>{message.user}</span>
-              {/* <span className='message-content'>{renderMessage}<a href={endOfArray}><img src={endOfArray} className='img'/></a></span> */}
-              <span className='message-content'>{renderMessage}</span>
-              <span className='message-image'><a href={endOfArray}><img src={endOfArray} className='img'/></a></span>
-              <span className='message-time'>{message.date}</span>
-            </div>
-          );
-        
-        // Else return without image tag in message-content
-        } else {
-
-          return (
-            <div key={message.id} className='message'>
-              <span className='message-username' style={divStyle}>{message.user}</span>
-              <span className='message-content'>{message.text}</span>
-              <span className='message-time'>{message.date}</span>
-            </div>
-          );
-        };
-
-      // If message is of type 'incomingNotification' then italicize and display different
-      } else if (message.type === 'incomingNotification'){
-
-        return (
-          <div key={message.id} className='message system'>
-            {message.oldUser} has changed name to {message.user}
-          </div>
-        );
-      }
-    });
-
-    return(
-      <div className='messages'>
-        {messages}
+class Loader extends Component {
+  render() {
+    return (
+      <div>
+        <div className="loader"></div>
       </div>
-    );
+    )
+  }
+}
+
+class MessageList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      console.log('simulating loading');
+      this.setState({loading: false})
+    }, 3000);
+  }
+
+  render(){
+
+    if (this.state.loading) {
+      return <Loader/>;
+    } else {
+      const messages = this.props.messages.map((message) => {
+
+        // Style for username color
+        let divStyle = {
+          color: message.color
+        };
+        
+        if (message.type === 'incomingMessage') {
+
+          // Convert incoming message text into an array
+          let arrayOfText = message.text.split(' ');
+
+          // Find the last value of the message text array
+          let endOfArray = arrayOfText[arrayOfText.length -1];
+
+          // Image Extension Variables
+          let gif = 'gif';
+          let png = 'png';
+          let jpg = 'jpg';
+
+          // If last text element ends with one of Image Extension Variables
+          if (endOfArray.endsWith(gif) || endOfArray.endsWith(png) || endOfArray.endsWith(jpg)) {
+
+            // Variable to remove link from text
+            let sliceArray = arrayOfText.slice(0, -1)
+
+            // Message without the image URL attached
+            let renderMessage = sliceArray.join(' ');
+
+            return (
+              <div key={message.id} className='message'>
+                <span className='message-username' style={divStyle}>{message.user}</span>
+                <span className='message-content'>
+                  <p>{renderMessage}</p>
+                  <a href={endOfArray}><img src={endOfArray} className='img'/></a>
+                </span>
+                {/* <span className='message-content'>{renderMessage}</span> */}
+                {/* <span className='message-image'><a href={endOfArray}><img src={endOfArray} className='img'/></a></span> */}
+                <span className='message-time'>{message.date}</span>
+              </div>
+            );
+          
+          // Else return without image tag in message-content
+          } else {
+
+            return (
+              <div key={message.id} className='message'>
+                <span className='message-username' style={divStyle}>{message.user}</span>
+                <span className='message-content'>{message.text}</span>
+                <span className='message-time'>{message.date}</span>
+              </div>
+            );
+          };
+
+        // If message is of type 'incomingNotification' then italicize and display different
+        } else if (message.type === 'incomingNotification'){
+
+          return (
+            <div key={message.id} className='message system'>
+              {message.oldUser} has changed name to {message.user}
+            </div>
+          );
+        }
+      });
+
+      return(
+        <div className='messages'>
+          {messages}
+        </div>
+      );
+    }
   }
 }
 
